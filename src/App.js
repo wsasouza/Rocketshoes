70dcd27da3b0f724f6c1060acbc578318ca26bd7
Walter Souza <wsasouza@hotmail.com>
2019-08-27 07:34:56 -0300

    Flux - Falha consumo API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-/*
-import "./App.css";
-*/
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>Hello World.</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
